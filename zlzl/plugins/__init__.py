8385a0af8aee0eb1b7e99502adff9ff82401888b
tomsmm29-oss <tomsmm29@gmail.com>
2026-09-08 16:38:26 +0300

    Update __init__.py

--- zlzl/plugins/__init__.py
+++ zlzl/plugins/__init__.py
@@ -1,4 +1,3 @@
-# عير بكسم معتز
 import math
 import os
 import re
@@ -13,7 +12,7 @@ from validators.url import url
 from .. import *
 from ..Config import Config
 from ..core.logger import logging
-from ..core.managers import edit_delete, edit_or_reply
+from ..core.managers import admin_cmd, edit_delete, edit_or_reply
 from ..core.session import zedub
 from ..helpers import *
 from ..helpers.utils import _format, _zedtools, _zedutils, install_pip, reply_id
